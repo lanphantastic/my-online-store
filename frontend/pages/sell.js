ee3750c1a7e46293970efb13ab34980956864f37
Lan Phan <lanc_phan@hotmail.com>
2020-03-30 21:22:40 -0400

    import 'CreateItem' component to the sell.js page

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,11 +1,8 @@
-import Link from 'next/link';
+import CreateItem from '../components/CreateItem';
 
 const Sell = props => (
 	<div>
-		<p>Sell!</p>
-		<Link href="/">
-			<a>Home!</a>
-		</Link>
+		<CreateItem />
 	</div>
 );
 
