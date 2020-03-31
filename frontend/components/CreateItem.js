81c556d8c37d03e5d082c0158439dd5bec3a724a
Lan Phan <lanc_phan@hotmail.com>
2020-03-30 21:52:07 -0400

    add the mutation variable to the createItem component along with the Router to redirect the user to the newly, created item page

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
+
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+	mutation CREATE_ITEM_MUTATION(
+		$title: String!
+		$description: String!
+		$price: Int!
+		$image: String
+		$largeImage: String
+	) {
+		createItem(
+			title: $title
+			description: $description
+			price: $price
+			image: $image
+			largeImage: $largeImage
+		) {
+			id
+			title
+		}
+	}
+`;
 
 class CreateItem extends Component {
 	state = {
@@ -24,51 +48,67 @@ class CreateItem extends Component {
 	render() {
 		const { title, description, image, largeImage, price } = this.state;
 		return (
-			<Form onSubmit={(e) => {
-        e.preventDefault();
-        
-      }}>
-				<fieldset>
-					<label htmlFor="title">
-						Title
-						<input
-							type="text"
-							id="title"
-							name="title"
-							placeholder="Title"
-							required
-							value={title}
-							onChange={this.handleChange}
-						/>
-					</label>
-					<label htmlFor="price">
-						Price
-						<input
-							type="number"
-							id="price"
-							name="price"
-							placeholder="Price"
-							required
-							value={price}
-							onChange={this.handleChange}
-						/>
-					</label>
-					<label htmlFor="description">
-						Description
-						<textarea
-							id="description"
-							name="description"
-							placeholder="Enter a description"
-							required
-							value={description}
-							onChange={this.handleChange}
-						/>
-					</label>
-          <button type="submit">Submit</button>
-				</fieldset>
-			</Form>
+			<Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+				{(createItem, { loading, error }) => (
+          
+					<Form
+						onSubmit={async e => {
+              // Stop the from from submitting
+              e.preventDefault();
+              // call the mutation
+              const response = await createItem();
+              // redirect the page to the item page
+              Router.push({
+                pathname: '/item',
+                query: { id: response.data.createItem.id }
+              })
+						}}
+					>
+            <Error error={error} />
+						<fieldset disabled={loading} aria-busy={loading}>
+							<label htmlFor="title">
+								Title
+								<input
+									type="text"
+									id="title"
+									name="title"
+									placeholder="Title"
+									required
+									value={title}
+									onChange={this.handleChange}
+								/>
+							</label>
+							<label htmlFor="price">
+								Price
+								<input
+									type="number"
+									id="price"
+									name="price"
+									placeholder="Price"
+									required
+									value={price}
+									onChange={this.handleChange}
+								/>
+							</label>
+							<label htmlFor="description">
+								Description
+								<textarea
+									id="description"
+									name="description"
+									placeholder="Enter a description"
+									required
+									value={description}
+									onChange={this.handleChange}
+								/>
+							</label>
+							<button type="submit">Submit</button>
+						</fieldset>
+					</Form>
+				)}
+			</Mutation>
 		);
 	}
 }
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
