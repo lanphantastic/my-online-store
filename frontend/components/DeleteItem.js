95903c0bf99efbcc80a2b030913b4a531e12f829
Lan Phan <lanc_phan@hotmail.com>
2020-04-05 18:56:32 -0400

    more fixing with indentation

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-
 import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
@@ -14,24 +13,22 @@ const DELETE_ITEM_MUTATION = gql`
 
 class DeleteItem extends Component {
   update = (cache, payload) => {
-    // manually update the cahce on the client, so it matches the server
+    // manually update the cache on the client, so it matches the server
     // 1. Read the cache for the items we want
     const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data, payload);
     // 2. Filter the deleted item out of the page
     data.items = data.items.filter(
       (item) => item.id !== payload.data.deleteItem.id
     );
-    // 3. Put the item back!
+    // 3. Put the items back!
     cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
   };
-
   render() {
     return (
       <Mutation
         mutation={DELETE_ITEM_MUTATION}
-        variables={{
-          id: this.props.id,
-        }}
+        variables={{ id: this.props.id }}
         update={this.update}
       >
         {(deleteItem, { error }) => (
