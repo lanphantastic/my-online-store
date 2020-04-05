fdce1e435d770c6ff939f1e253622e41527d43e8
Lan Phan <lanc_phan@hotmail.com>
2020-04-05 17:23:54 -0400

    STYLE: Github was treating the indentation as tabs or longer so by opening the command palette and typing 'convert indentation to spaces' fixed the problem

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,32 +1,32 @@
 const Mutations = {
-	async createItem(parent, args, context, info) {
-		// TODO: check if they are logged in
-		const item = await context.db.mutation.createItem(
-			{
-				data: {
-					...args,
-				},
-			},
-			info
-		);
-		return item;
-	},
-	updateItem(parent, args, context, info) {
-		// first take a copy of the updates
-		const updates = { ...args };
-		// remove the ID from the updates
-		delete updates.id;
-		// run the update method
-		return context.db.mutation.updateItem(
-			{
-				data: updates,
-				where: {
-					id: args.id,
-				},
-			},
-			info
-		);
-	},
+  async createItem(parent, args, context, info) {
+    // TODO: check if they are logged in
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
+  },
+  updateItem(parent, args, context, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return context.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
