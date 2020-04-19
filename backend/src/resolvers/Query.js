782a0d78a10058932667a54561b8efc4658e3cc5
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 16:24:23 -0400

    query for all items with connections

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,12 +1,13 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-	items: forwardTo('db'),
-	item: forwardTo('db'),
-	// async items(parent, args, context, info) {
-	//   const items = await context.db.query.items();
-	//   return items;
-	// }
+  items: forwardTo('db'),
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
+  // async items(parent, args, context, info) {
+  //   const items = await context.db.query.items();
+  //   return items;
+  // }
 };
 
 module.exports = Query;
