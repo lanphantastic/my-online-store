887ee2d1d3a3995f177bef9a7a76c5f67dcb0fb9
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 15:52:19 -0400

    import styled-components

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import styled from 'styled-components';
+
+import Error from './ErrorMessage';
+
+const SingleItemStyle = styled.div`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+`;
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
