7093be1e858faf38b7fcd05d39396892f460c957
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 16:01:21 -0400

    css the image and title and description

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -14,6 +14,15 @@ const SingleItemStyle = styled.div`
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
   min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `;
 
 const SINGLE_ITEM_QUERY = gql`
