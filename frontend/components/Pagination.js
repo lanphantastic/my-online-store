f1ae88e3caf84d3157cbf99da70d78390689b2d9
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 16:10:20 -0400

    create a pagination block

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+const Pagination = () => (
+  <PaginationStyles>
+    <p>Hi i'm pagination</p>
+  </PaginationStyles>
+);
+
+export default Pagination;
