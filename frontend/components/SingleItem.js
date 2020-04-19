bb1ec9ed5d21ef31e1a4a8751356b2f947834652
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 15:28:18 -0400

    made a single item page and component

---
+++ frontend/components/SingleItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class SingleItem extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Single item component</p>
+      </div>
+    )
+  }
+}
+
+export default SingleItem;
