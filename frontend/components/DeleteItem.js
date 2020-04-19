0865ebaf3ee736e1b1856ca0e6886694973e61f1
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 15:21:15 -0400

    fix the deleteItem with cache

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -16,13 +16,12 @@ class DeleteItem extends Component {
     // manually update the cache on the client, so it matches the server
     // 1. Read the cache for the items we want
     const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
-    console.log(data, payload);
     // 2. Filter the deleted item out of the page
-    data.items = data.items.filter(
-      (item) => item.id !== payload.data.deleteItem.id
+    const items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
     );
     // 3. Put the items back!
-    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data: { items } });
   };
   render() {
     return (
