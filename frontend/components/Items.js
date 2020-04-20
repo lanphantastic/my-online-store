4a589c920001a54595fb5c6a9e645e0b51e77cbe
Lan Phan <lanc_phan@hotmail.com>
2020-04-19 20:33:19 -0400

    pass page props down to Pagination component and fix the indentation

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -7,52 +7,52 @@ import Pagination from './Pagination';
 import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
-			id
-			title
-			price
-			description
-			image
-			largeImage
-		}
-	}
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
 `;
 
 const Center = styled.div`
-	text-align: center;
+  text-align: center;
 `;
 
 const ItemsList = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-gap: 60px;
-	max-width: ${props => props.theme.maxWidth};
-	margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
 `;
 
 class Items extends Component {
-	render() {
-		return (
-			<Center>
-				<Pagination></Pagination>
-				<Query query={ALL_ITEMS_QUERY}>
-					{({ data, error, loading }) => {
-						if (loading) return <p>Loading...</p>;
-						if (error) return <p>Error: {error.message}</p>;
-						return (
-							<ItemsList>
-								{data.items.map(item => (
-									<Item key={item.id} item={item} />
-								))}
-							</ItemsList>
-						);
-					}}
-				</Query>
-				<Pagination></Pagination>
-			</Center>
-		);
-	}
+  render() {
+    return (
+      <Center>
+        <Pagination page={this.props.page}></Pagination>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item key={item.id} item={item} />
+                ))}
+              </ItemsList>
+            );
+          }}
+        </Query>
+        <Pagination page={this.props.page}>></Pagination>
+      </Center>
+    );
+  }
 }
 
 export default Items;
