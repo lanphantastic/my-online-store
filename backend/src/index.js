0651d8772412d14d4b4873cd1048371ec3425a67
Lan Phan <lanc_phan@hotmail.com>
2020-04-21 22:43:45 -0400

    pass 'cookieParser' to express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,5 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -6,6 +8,7 @@ const server = createServer();
 
 
 // TODO: Use express middleware to handle cookies {JWT}
+server.express.use(cookieParser());
 // TODO: Use express middleware to populate current users
 
 server.start(
