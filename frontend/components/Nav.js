085e7ff46bbd938c6fc73484ad92bc9cbc0fd0f4
Lan Phan <lanc_phan@hotmail.com>
2020-04-22 21:54:57 -0400

    render user's info as a props to the navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({data: { me }}) => {
+        if(me) return <p>{me.name}</p>
+        return <p>User</p>;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
