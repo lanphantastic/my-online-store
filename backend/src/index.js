035dfb9854bf1386bd81f2ad7eca538db99f626f
Lan Phan <lanc_phan@hotmail.com>
2020-04-22 21:43:32 -0400

    get the current login user and verify their cookies with JWT

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -10,6 +11,16 @@ const server = createServer();
 // TODO: Use express middleware to handle cookies {JWT}
 server.express.use(cookieParser());
 // TODO: Use express middleware to populate current users
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if(token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+})
 
 server.start(
   {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,10 +4,18 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
-  // async items(parent, args, context, info) {
-  //   const items = await context.db.query.items();
-  //   return items;
-  // }
+  me(parent, args, context, info) {
+    // check if there is a current user ID
+    if (!context.request.userId) {
+      return null;
+    }
+    return context.db.query.user(
+      {
+        where: { id: context.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
