17ec70bb709c93f3e1c5dfc018e7adc69ed1b1ac
Lan Phan <lanc_phan@hotmail.com>
2020-04-26 19:05:30 -0400

    use 'transports' and 'makeANiceEmail' to send the request reset token email to MailTrap for the time being and a link to the reset password page

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 
+const { transport, makeANiceEmail } = require('../mail');
+
 const Mutations = {
   async createItem(parent, args, context, info) {
     // TODO: check if they are logged in
@@ -107,8 +109,19 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken, resetTokenExpiry },
     });
-    return {message: 'Thanks!'};
     // 3. Email them that reset token
+    const mailRes = await transport.sendMail({
+      from: 'wes@wesbos.com',
+      to: user.email,
+      subject: 'Your Password Reset Token',
+      html: makeANiceEmail(`Your Password Reset Token is here!
+      \n\n
+      <a href="${process.env
+        .FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+    });
+
+    // 4. Return the message
+    return { message: 'Thanks!' };
   },
   async resetPassword(parent, args, context, info) {
     // 1. Check if the passwords match
