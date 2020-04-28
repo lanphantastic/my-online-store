85683a416463ee18547ce75a211a10867abacca0
Lan Phan <lanc_phan@hotmail.com>
2020-04-27 21:13:22 -0400

    video 34 - create a middleware that populates the user on each request

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,20 +7,31 @@ const db = require('./db');
 
 const server = createServer();
 
-
-// TODO: Use express middleware to handle cookies {JWT}
 server.express.use(cookieParser());
-// TODO: Use express middleware to populate current users
+
 // decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
-  if(token) {
+  if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     // put the userId onto the req for future requests to access
     req.userId = userId;
   }
   next();
-})
+});
+
+// 2. Create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, permissions, email, name }'
+  );
+  req.user = user;
+  next();
+});
 
 server.start(
   {
