0a4d64966d0b1163b94e73803a8a10bc345127f6
Lan Phan <lanc_phan@hotmail.com>
2020-04-30 14:13:22 -0400

    feat(frontend): loop through the items in the cart with the CartItem component that displays the image, title, description, and price

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+
 import User from './User';
+import CartItem from './CartItem';
 
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
+
+import calcTotalPrice from '../lib/calcTotalPrice';
+import formatMoney from '../lib/formatMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -24,6 +29,7 @@ const Cart = () => (
   <User>
     {({ data: { me } }) => {
       if (!me) return null;
+      console.log(me)
       return (
         <Mutation mutation={TOGGLE_CART_MUTATION}>
           {toggleCart => (
@@ -36,18 +42,14 @@ const Cart = () => (
                     </CloseButton>
                     <Supreme>{me.name}'s Cart</Supreme>
                     <p>
-                      You have {me.cart.length} item
-                      {me.cart.length === 1 ? '' : 's'} in your cart
+                      You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.
                     </p>
                   </header>
                   <ul>
-                    {me.cart.map(cartItem => <li>
-                      {cartItem.id}
-                    </li>)}
+                    {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}
                   </ul>
-
                   <footer>
-                    <p>$10.10</p>
+                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
                     <SickButton>Checkout</SickButton>
                   </footer>
                 </CartStyles>
