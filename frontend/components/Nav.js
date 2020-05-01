2ea7f61187a49567acb30019cd7bb5f9ce17a237
Lan Phan <lanc_phan@hotmail.com>
2020-05-01 13:22:46 -0400

    show total quantity of items beside the 'My Cart'

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -6,6 +6,7 @@ import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 const Nav = () => (
   <User>
@@ -27,8 +28,16 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {(toggleCart) => (
-                <button onClick={toggleCart}>My Cart</button>
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart{' '}
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
               )}
             </Mutation>
           </>
@@ -37,7 +46,6 @@ const Nav = () => (
           <Link href="/signup">
             <a>Sign In</a>
           </Link>
-
         )}
       </NavStyles>
     )}
