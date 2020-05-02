a43a83b461482df385b2017ba5e7039dab2869a6
Lan Phan <lanc_phan@hotmail.com>
2020-05-01 20:27:47 -0400

    search dropdown autocomplete

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+
 import Nav from './Nav';
 import Cart from './Cart';
+import Search from './Search';
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -65,7 +68,7 @@ const Header = () => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>Search</p>
+      <Search />
     </div>
     <Cart />
   </StyledHeader>
