04480552a9e4242ff34f10b94486609121190955
Lan Phan <lanc_phan@hotmail.com>
2020-05-21 11:26:01 -0400

    create a 'order' and 'orders' query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -28,6 +28,43 @@ const Query = {
     // 2. if they do, query all the users!
     return ctx.db.query.users({}, info);
   },
+  async order(parent, args, context, info) {
+    // 1. Make sure they are logged in
+    if (!context.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 2. Query the current order
+    const order = await context.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === context.request.userId;
+    const hasPermissionToSeeOrder = context.request.user.permissions.includes(
+      'ADMIN'
+    );
+    if (!ownsOrder || !hasPermission) {
+      throw new Error('You are not allowed to this!');
+    }
+    // 4. return the order
+    return order;
+  },
+  async orders(parent, args, context, info) {
+    const { userId } = context.request;
+    if (!userId) {
+      throw new Error('You must be logged in!');
+    }
+    return context.db.query.orders(
+      {
+        where: {
+          user: { id: userId },
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
