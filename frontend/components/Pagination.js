d7e4a494c79b6e1b4b1e7398b0f552ae11bbf804
Lan Phan <lanc_phan@hotmail.com>
2020-05-24 18:30:35 -0400

    add a data-test class and export the pagination query

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -25,7 +25,7 @@ const Pagination = props => (
       const pages = Math.ceil(count / perPage);
       const page = props.page;
       return (
-        <PaginationStyles>
+        <PaginationStyles data-test="pagination">
           <Head>
             <title>
               Sick Fits! — Page {page} of {pages}
@@ -64,3 +64,4 @@ const Pagination = props => (
 );
 
 export default Pagination;
+export { PAGINATION_QUERY };
